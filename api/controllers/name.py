acdcc404ff3d0f3e0932ac4aabd1271ea3fd5fa5
Yorn90104 <bingxiao526@gmail.com>
2025-04-19 01:07:49 +0800

    feature: 查詢名稱顯示簡稱

--- api/controllers/name.py
+++ api/controllers/name.py
@@ -8,17 +8,21 @@ def controller(text: str) -> list[SendMessage]:
     # 解析使用者輸入的文字，取得股票代號
     part = text.split(" ")
     stock_no = part[1]
+    
+    stock = TaiwanStockExchangeCrawler.no(stock_no)
 
     # 查詢股票名稱
-    stock_name = TaiwanStockExchangeCrawler.no(stock_no).get("股票全名")[0]
-
+    stock_full_name = stock.get("股票全名")[0]
+    stock_short_name = stock.get("股票簡稱")[0] if stock.get("股票簡稱") else "無資料"
+    
     # 回覆訊息列表
     return [
             TextSendMessage(
                 text=(
                     f"🔍 查詢股票名稱\n"
                     f"📌 股票代號：{stock_no}\n"
-                    f"📘 股票名稱：{stock_name}"
+                    f"🏢 股票名稱：{stock_full_name}\n"
+                    f"📘 股票簡稱：{stock_short_name}"
                 )
             )
         ]
